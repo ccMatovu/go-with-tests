6a362acc1d3498a36ed2c66e898cee23e4f2d0fd
ccm-chucky <charlescmatovu@gmail.com>
2021-12-03 13:44:00 -0500

    go with tests startup and hello world

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,19 +3,30 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const frenchHelloPrefix = "Bonjour, "
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 
-func Hello(name,language string) string{
-	if (name == ""){
-		return englishHelloPrefix + "World"
-	}else if (language == spanish){
-		return spanishHelloPrefix + name
-	}else if(language == "French"){
-		return "Bonjour, "+name
-	}
-	return englishHelloPrefix + name
+ 
+func Hello(name string, language string) string {
+    if name == "" {
+        name = "World"
+    }
+    return greetingPrefix(language) + name
 }
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+        prefix = spanishHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    return
+}
+
 func main() {
 	fmt.Println(Hello("world",""))
 }
