a2f0fc3d20c41e7dc655930f5595cdeff59145d1
ccm-chucky <charlescmatovu@gmail.com>
2021-12-04 20:28:17 -0500

    arrays SumAll() and test

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) int{
 	var sum int;
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+func SumAll(slicesToSum ...[]int) []int{
+	fmt.Println("checkckckckck")
+	slice := []int{}
+
+	for _,v := range slicesToSum{
+		var sum int
+		for _, value := range v {
+			sum += value
+		}
+
+		slice = append(slice,sum )
+
+	}
+	return slice
 }
