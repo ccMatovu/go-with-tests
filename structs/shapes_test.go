bf2c78401f63eeaa07c0f59bfc3e4e7ea51a9491
ccm-chucky <charlescmatovu@gmail.com>
2021-12-06 19:04:36 -0500

    structs and methods

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,6 +13,10 @@ type Circle struct{
 	Radius float64
 }
 
+type Shape interface{
+	Area() float64
+}
+
 func TestPerimeter(t *testing.T){
 	t.Run("testing the perimeter",func(t *testing.T) {
 		rectangle := Rectangle{10.0,10.0}
@@ -28,27 +32,29 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T){
+
+	checkArea := func (t *testing.T,shape Shape,want float64)  {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want{
+			t.Errorf("got %g but expected %g",got,want)
+		}
+	}
+
 	t.Run("test the area Rectangle",func(t *testing.T) {
 		rectangle := Rectangle{
 						Width: 12.0,
 						Height: 6.0,
 					}
-		got := rectangle.Area()
 		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f but expected %.2f",got,want)
-		}
+		checkArea(t,rectangle,want)
 	})
 	
 	t.Run("testing circle area",func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
         want := 314.1592653589793
-
-        if got != want {
-            t.Errorf("got %g want %g", got, want)
-        }
+		checkArea(t,circle,want)
 
 	})
 }
